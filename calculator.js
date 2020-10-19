92c482107da2c932944b916e7444615d3487a4e7
mohammadhasan3 <mohamed.hassan940@hotmail.com>
2020-10-19 17:03:16 +0300

    Designed calculator using if statements and pre defined numbers only

--- calculator.js
+++ calculator.js
@@ -1,3 +1,17 @@
 // Define the numbers and the operation here
-
+let first_int = 5;
+let second_int = 10;
+let operation = "/";
 // Write if-statements here
+
+if(operation === "+"){
+    console.log(first_int + second_int);
+}else
+if(operation === "-"){
+    console.log(first_int - second_int);
+}else
+if(operation === "*"){
+    console.log(first_int * second_int);
+}else{
+    console.log(first_int / second_int);
+}
